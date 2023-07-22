987cd19ea7e01abaa00936adf3b913dea797ce23
MinseoKangQ <kms02171@naver.com>
2023-07-22 22:16:33 +0900

    프로젝트 생성 & DB 연결

---
+++ src/main/java/com/server/sumnote/SumNoteApplication.java
@@ -0,0 +1,13 @@
+package com.server.sumnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumNoteApplication.class, args);
+	}
+
+}
