6142b0ab13164c95290b0cd7236eae67c416eebe
MinseoKangQ <kms02171@naver.com>
2023-08-04 23:18:52 +0900

    Controller 추가, Swagger 추가 작성

--- src/main/java/com/server/sumnote/quiz/controller/Controller.java
+++ src/main/java/com/server/sumnote/quiz/controller/Controller.java
@@ -1,28 +1,65 @@
 package com.server.sumnote.quiz.controller;
 
+import com.server.sumnote.quiz.entity.Quiz;
 import com.server.sumnote.quiz.service.ChatServiceImpl;
+import com.server.sumnote.user.entity.User;
 import io.github.flashvayne.chatgpt.service.ChatgptService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
-@RestController
+import java.util.List;
+
+
+
 @Slf4j
-@RequestMapping("/make-quiz")
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 @Component("quizController")
+@Api(tags = {"Quiz API"})
 public class Controller {
+
     private final ChatServiceImpl chatServiceImpl;
     private final ChatgptService chatgptService;
 
-    //chat-gpt 와 간단한 채팅 서비스 소스
-    @PostMapping("")
+    @PostMapping("/make-quiz")
+    @ApiOperation(value = "chat-gpt 와 연동하여 문제&답변 생성")
     public String makeQuiz(@RequestBody String question) {
         return chatServiceImpl.getChatResponse(question);
+    }
+
+    @GetMapping("/quizzes")
+    @ApiOperation(value = "유저의 모든 퀴즈 문서 보여주기")
+    public List<Quiz> getAllQuizzes(@RequestBody User user) {
+
+        // 유저의 아이디에 해당하는 퀴즈 문서들 가져오기
+
+        // 일단 null
+        return null;
+    }
+
+    // 터치한 문서의 번호로 접근
+    @GetMapping("/quizzes-detail")
+    @ApiOperation(value = "퀴즈 문서 조회")
+    public Quiz getQuiz(@RequestBody Long id) {
+
+        // 문서 아이디에 해당하는 퀴즈 문서 하나 가져오기
+
+        // 일단 null
+        return null;
+    }
+
+    // 터치한 문서의 번호로 접근
+    @DeleteMapping("/quiz")
+    @ApiOperation(value = "퀴즈 문서 삭제")
+    public void deleteQuiz(@PathVariable Long id) {
+
+        // 문서 아이디에 해당하는 퀴즈 문서 하나 가져와서 삭제
+
     }
 
 }
--- src/main/java/com/server/sumnote/summary/controller/Controller.java
+++ src/main/java/com/server/sumnote/summary/controller/Controller.java
@@ -1,28 +1,62 @@
 package com.server.sumnote.summary.controller;
 
+import com.server.sumnote.quiz.entity.Quiz;
 import com.server.sumnote.summary.service.ChatServiceImpl;
+import com.server.sumnote.user.entity.User;
 import io.github.flashvayne.chatgpt.service.ChatgptService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-@RequiredArgsConstructor
-@RestController
 @Slf4j
-@RequestMapping("/sum-note")
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 @Component("summaryController")
+@Api(tags = {"Summary API"})
 public class Controller {
+
     private final ChatServiceImpl chatServiceImpl;
     private final ChatgptService chatgptService;
 
-    //chat-gpt 와 간단한 채팅 서비스 소스
-    @PostMapping("")
+    @PostMapping("/sum-note")
+    @ApiOperation(value = "chat-gpt 와 연동하여 요약본 생성")
     public String makeSumNote(@RequestBody String question) {
         return chatServiceImpl.getChatResponse(question);
+    }
+
+    @GetMapping("/sum-notes")
+    @ApiOperation(value = "유저의 모든 요약 노트 보여주기")
+    public List<Quiz> getAllSumNotes(@RequestBody User user) {
+
+        // 유저의 아이디에 해당하는 요약 문서들 가져오기
+
+        // 일단 null
+        return null;
+    }
+
+    // 터치한 문서의 번호로 접근
+    @GetMapping("/sum-notes-detail")
+    @ApiOperation(value = "요약 노트 조회")
+    public Quiz getSumNote(@RequestBody Long id) {
+
+        // 문서 아이디에 해당하는 요약 문서 하나 가져오기
+
+        // 일단 null
+        return null;
+    }
+
+    // 터치한 문서의 번호로 접근
+    @DeleteMapping("sum-notes")
+    @ApiOperation(value = "요약 노트 삭제")
+    public void deleteSumNote(@PathVariable Long id) {
+
+        // 문서 아이디에 해당하는 요약 문서 하나 가져와서 삭제
+
     }
 
 }
