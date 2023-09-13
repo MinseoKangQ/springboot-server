9257c8135ff0214c048dff5f525c94be180307ec
MinseoKangQ <kms02171@naver.com>
2023-09-13 15:21:53 +0900

    Quiz 엔티티 작성 완료

--- src/main/java/com/server/sumnote/quiz/entity/Quiz.java
+++ src/main/java/com/server/sumnote/quiz/entity/Quiz.java
@@ -25,32 +25,31 @@ public class Quiz {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "quiz_id")
-    private Long id;
+    private Long id; // 자동 생성되는 기본키
 
-    // FK
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
+    private User user; // 유저 외래키
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sum_id")
-    private Summary summary;
+    private Summary summary; // 퀴즈에 해당하는 요약노트
 
-    private String question;
+    private String question; // 퀴즈 내용
 
-    private String selections;
+    private String selections; // 퀴즈 선택지들 (4지선다)
 
-    private Integer answer;
+    private String answer; // 정답
 
-    private String commentary;
+    private String commentary; // 해설
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime created_at;
+    private LocalDateTime created_at; // 생성일
 
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime last_modified_at;
+    private LocalDateTime last_modified_at; // 수정일
 
     @Override
     public boolean equals(Object o) {
